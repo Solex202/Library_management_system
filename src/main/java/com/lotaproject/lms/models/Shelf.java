061b02da499744fb3c4cc3a3bb4853e022b3a85f
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-09-06 12:36:54 +0100

    creating models

---
+++ src/main/java/com/lotaproject/lms/models/Shelf.java
@@ -0,0 +1,11 @@
+package com.lotaproject.lms.models;
+
+import java.util.List;
+
+public class Shelf {
+
+    private String id;
+    private String name;
+    private List<Book> books;
+    private Folder folder;
+}
