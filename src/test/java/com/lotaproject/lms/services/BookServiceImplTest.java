4d2a979a46d5c0ae862259f6a4d6785dba727f01
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-09-06 16:56:25 +0100

    creating models

---
+++ src/test/java/com/lotaproject/lms/services/BookServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.lotaproject.lms.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class BookServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
