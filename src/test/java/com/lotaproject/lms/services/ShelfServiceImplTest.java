ea22eeb02b152c3283b8b3a25f4845e95ab050d3
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-09-07 23:03:43 +0100

    testing shelf service

---
+++ src/test/java/com/lotaproject/lms/services/ShelfServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.lotaproject.lms.services;
+
+import com.lotaproject.lms.models.Shelf;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ShelfServiceImplTest {
+
+    @Autowired
+    private ShelfService shelfService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createShelf() {
+        String name = "shelf 1";
+        String category = "Science books";
+
+        Shelf shelf = shelfService.createShelf(name, category);
+
+        assertNotNull(shelf);
+    }
+
+    @Test
+    void addBookToShelf() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        shelfService.addBookToShelf(1L ,ids);
+    }
+
+    @Test
+    void addFolderToShelf(){
+        shelfService.addFolderToShelf(1L, 1L);
+    }
+}
